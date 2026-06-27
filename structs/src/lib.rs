6a3cc2ed150cda75c34ac884ab1a92c636f827b2
BhJaipal <105307427+BhJaipal@users.noreply.codeberg.org>
2026-06-27 15:43:24 +0530

    test and drop, clone, into_iter, index

--- structs/src/lib.rs
+++ structs/src/lib.rs
@@ -43,4 +43,34 @@ mod tests {
             ind += 1;
         }
     }
+
+    #[test]
+    fn capacity() {
+        let mut res = Array::<i64>::with_capacity(4);
+        res.push(3);
+        res.push(2);
+        res.push(8);
+        res.push(9);
+        res.push(7);
+        assert_eq!(8, res.capacity());
+        res.pop();
+        assert_eq!(4, res.capacity());
+        res.pop();
+        assert_eq!(4, res.capacity(), "Capacity should remain 4");
+    }
+
+    #[test]
+    fn map() {
+        let res = data();
+        let mut ind = 0;
+        for i in res.clone().into_iter().map(|x| x * x) {
+            let d = res[ind];
+            assert_eq!(i, d * d);
+            ind += 1;
+        }
+
+        let out: Array<i64> = res.clone().into_iter().filter(|x| *x > 5).collect();
+        let len = out.len();
+        assert_eq!(len, 3);
+    }
 }
